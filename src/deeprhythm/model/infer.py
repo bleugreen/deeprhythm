37af859d331f05bc84c05066915561aea6e19e29
bleugreen <mitchell5799@gmail.com>
2024-12-01 01:52:31 -0500

    removed old func

--- src/deeprhythm/model/infer.py
+++ src/deeprhythm/model/infer.py
@@ -38,24 +38,3 @@ def predict_global_bpm(input_path, model_path='deeprhythm-0.7.pth', model=None, 
         _, predicted_class = torch.max(mean_probabilities, 0)
         predicted_global_bpm = class_to_bpm(predicted_class.item())
     return predicted_global_bpm, time.time()-start
-
-
-def predict_global_bpm_cont(input_path, model_path='deeprhythm-0.5.pth', model=None, specs=None, device='cuda'):
-    if model is None:
-        model = load_cnn_model(model_path, device=device)
-    clips = load_and_split_audio(input_path, sr=22050)
-    model_device = next(model.parameters()).device
-    if specs is None:
-        stft, band, cqt = make_kernels(device=model_device)
-    else:
-        stft, band, cqt = specs
-    input_batch = compute_hcqm(clips.to(device=model_device), stft, band, cqt).permute(0,3,1,2)
-    model.eval()
-    start = time.time()
-    with torch.no_grad():
-        input_batch = input_batch.to(device=model_device)
-        outputs = model(input_batch)
-        probabilities = torch.softmax(outputs, dim=1)
-        expected_bpm = torch.sum(probabilities * torch.arange(256).float().to(device), dim=1)
-    mean_expected_bpm = torch.mean(expected_bpm)
-    return mean_expected_bpm, time.time()-start
