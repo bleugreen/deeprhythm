e2033bd20591aaea5c5e73a72551b75b412d1c46
bleugreen <mitchell5799@gmail.com>
2026-02-21 22:52:03 -0500

    [Build] Fix hcqm.py: change default device to None, resolve with get_device()

--- src/deeprhythm/audio_proc/hcqm.py
+++ src/deeprhythm/audio_proc/hcqm.py
@@ -1,13 +1,16 @@
 import numpy as np
 import torch
 import nnAudio.features as feat
-from deeprhythm.audio_proc.bandfilter import create_log_filter, apply_log_filter
+
+from deeprhythm.audio_proc.bandfilter import apply_log_filter, create_log_filter
 from deeprhythm.audio_proc.onset import onset_strength
+from deeprhythm.utils import get_device
 
 N_BINS = 240
 N_BANDS = 8
 
-def make_kernels(len_audio=22050*8, sr=22050, device='cuda'):
+
+def make_kernels(len_audio=22050*8, sr=22050, device=None):
     """
     Create the kernels for the STFT and CQT based on the input parameters.
 
@@ -28,29 +31,33 @@ def make_kernels(len_audio=22050*8, sr=22050, device='cuda'):
     cqt_specs : list of CQT objects
         A list of Constant-Q Transform (CQT) objects for different harmonics.
     """
+    if device is None:
+        device = get_device()
     n_fft = 2048
     hop = 512
     n_fft_bins = int(1+n_fft/2)
     band_filter = create_log_filter(n_fft_bins, N_BANDS, device=device)
-    stft_spec = feat.stft.STFT(sr=sr, n_fft=n_fft, hop_length=hop, output_format='Magnitude', verbose=False).to(device=device)
+    stft_spec = feat.stft.STFT(
+        sr=sr, n_fft=n_fft, hop_length=hop, output_format='Magnitude', verbose=False
+    ).to(device=device)
     cqt_specs = []
     for h in [1/2, 1, 2, 3, 4, 5]:
-        # Convert from BPM to Hz
         fmin = (32.7*h)/60
         sr_cqt = len_audio//(hop*8)
-        fmax =sr_cqt/2
+        fmax = sr_cqt/2
         num_octaves = np.log2(fmax/fmin)
         bins_per_octave = N_BINS / num_octaves
         cqt_spec = feat.cqt.CQT(sr=sr_cqt,
-                                hop_length=len_audio//hop,
-                                n_bins=N_BINS,
-                                bins_per_octave=bins_per_octave,
-                                fmin=fmin,
-                                output_format='Magnitude',
-                                verbose=False,
-                                pad_mode='constant').to(device=device)
+                                 hop_length=len_audio//hop,
+                                 n_bins=N_BINS,
+                                 bins_per_octave=bins_per_octave,
+                                 fmin=fmin,
+                                 output_format='Magnitude',
+                                 verbose=False,
+                                 pad_mode='constant').to(device=device)
         cqt_specs.append(cqt_spec)
     return stft_spec, band_filter, cqt_specs
+
 
 def compute_hcqm(y, stft_spec, band_filter, cqt_specs):
     """
@@ -63,10 +70,10 @@ def compute_hcqm(y, stft_spec, band_filter, cqt_specs):
     - y (Tensor): The input signal tensor of shape (batch_size, num_samples).
     - stft_spec (STFT object): An object to compute the Short-Time Fourier Transform (STFT).
     - band_filter (Tensor): A filter matrix of shape (num_bands, num_bins) to apply to the STFT.
-    - cqt_specs (list of CQT objects): A list of Constant-Q Transform (CQT) objects for different harmonics / bands
+    - cqt_specs (list of CQT objects): A list of CQT objects for different harmonics / bands
 
     Returns:
-    - hcqm (Tensor): The computed HCQM of shape (batch_size, N_BINS, N_BANDS, N_HARMONICS), where 6 corresponds to the number of different harmonics analyzed.
+    - hcqm (Tensor): The computed HCQM of shape (batch_size, N_BINS, N_BANDS, N_HARMONICS)
     """
     stft = stft_spec(y)
     stft_bands = apply_log_filter(stft, band_filter)
